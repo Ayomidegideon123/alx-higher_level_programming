2f6839f2bf43a125895d2ba76834dd8ec5c92751
Akanji Ayomide Gideon <110993463+Ayomidegideon123@users.noreply.github.com>
2022-12-01 22:08:23 +0100

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+
+    p = len(sys.argv) - 1
+    if p == 0:
+        print("0 arguments.")
+    elif p == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(p))
+    for x in range(p):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
